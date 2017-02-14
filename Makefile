afe76d83a2ad0790931ff27fe4daffb6374e3655
Paul Downey <psd@whatfettle.com>
2017-02-14 19:38:49 +0000

    Fold in Companies House resources

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
-data/industry/industry.tsv:	bin/industry.py lists/sic2007/list.tsv
+LISTS=\
+	lists/uksic2007/list.tsv \
+	lists/companies-house-resources/list.tsv
+
+data/industry/industry.tsv:	bin/industry.py $(LISTS)
 	mkdir -p data/industry
-	python3 bin/industry.py < lists/sic2007/list.tsv > $@
+	python3 bin/industry.py $(LISTS) > $@
--- bin/industry.py
+++ bin/industry.py
@@ -9,23 +9,31 @@ sep = '\t'
 
 section = ''
 
-print(sep.join(fields))
+industry = {}
 
-for row in csv.DictReader(sys.stdin, delimiter=sep):
 
-    if len(row['industry']) == 1:
-        section = row['industry']
-        row['parent-industry'] = ''
-    else:
-        code = re.sub("\D", "", row['industry'])
-        if len(code) == 2:
-            row['parent-industry'] = section
+for path in sys.argv[1:]:
+    for row in csv.DictReader(open(path), delimiter=sep):
+
+        if len(row['industry']) == 1:
+            section = row['industry']
+            row['parent-industry'] = ''
         else:
-            row['parent-industry'] = code[:-1]
+            code = re.sub("\D", "", row['industry'])
+            if len(code) == 2:
+                row['parent-industry'] = section
+            else:
+                row['parent-industry'] = code[:-1]
 
-        row['industry'] = code
+            row['industry'] = code
 
-        # expand abbreviations
-        row['name'] = row['name'].replace('n.e.c.', 'not elsewhere classified')
+            # expand abbreviations
+            row['name'] = row['name'].replace('n.e.c.', 'not elsewhere classified')
 
+        industry[row['industry']] = row
+
+
+print(sep.join(fields))
+for code in sorted(industry):
+    row = industry[code]
     print(sep.join([row.get(field, '') for field in fields]))
