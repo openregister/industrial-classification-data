68ad4122885ab79c86323389f6293252f9a26a7c
Paul Downey <psd@whatfettle.com>
2017-02-16 23:04:10 +0000

    Improved parenting
    make four digit codes end with a '0' to match Companies House lower codes and blank missing parents.
    This highlights the codes added by Companies House.

--- bin/industrial-classification.py
+++ bin/industrial-classification.py
@@ -10,25 +10,42 @@ sep = '\t'
 section = ''
 
 industrial_classification = {}
+parents = {}
 
-
+# read lists from the command line
 for path in sys.argv[1:]:
     for row in csv.DictReader(open(path), delimiter=sep):
 
-        if len(row['industrial-classification']) == 1:
-            section = row['industrial-classification']
-            row['parent-industrial-classification'] = ''
+        code = row['industrial-classification']
+
+        # set section
+        if len(code) == 1:
+            section = code
+            parent = ''
         else:
+            # compress punctuation from the code
             code = re.sub("\D", "", row['industrial-classification'])
+
+            # make codes match those of Companies House
+            if len(code) == 4:
+                code = code + '0'
+
+            # deduce parent code
             if len(code) == 2:
-                row['parent-industrial-classification'] = section
+                parent = section
+            elif len(code) == 5:
+                if code[-1] == '0':
+                    parent = code[:-2]
+                else:
+                    parent = code[:-1] + '0'
             else:
-                row['parent-industrial-classification'] = code[:-1]
+                parent = code[:-1]
 
-            row['industrial-classification'] = code
+        row['parent-industrial-classification'] = parent
+        row['industrial-classification'] = code
 
-            # expand abbreviations
-            row['name'] = row['name'].replace('n.e.c.', 'not elsewhere classified')
+        # expand abbreviations
+        row['name'] = row['name'].replace('n.e.c.', 'not elsewhere classified')
 
         industrial_classification[row['industrial-classification']] = row
 
@@ -36,4 +53,9 @@ for path in sys.argv[1:]:
 print(sep.join(fields))
 for code in sorted(industrial_classification):
     row = industrial_classification[code]
+
+    parent = row['parent-industrial-classification']
+    if parent and parent not in industrial_classification:
+        row['parent-industrial-classification'] = ''
+
     print(sep.join([row.get(field, '') for field in fields]))
